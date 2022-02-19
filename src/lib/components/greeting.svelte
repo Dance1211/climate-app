b113a21c5cfd07f509c017c2cb0ec77d30958a22
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-19 13:57:05 +0000

    Rejig the front page to suite start progress

---
+++ src/lib/components/greeting.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let clicks = 0;
+
+	const handleClick = (): void => {
+		clicks += 1;
+	};
+
+</script>
+
+<h3>Hello! Welcome to the weather app!</h3>
+
+<button on:click={handleClick}>
+  {clicks}
+</button>
