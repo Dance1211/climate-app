f5bb4bdcc7eb7ea57456261bd7a5d220a080404f
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-19 23:25:27 +0000

    Move mongo db connection to services

--- src/lib/services/db/mongodb.ts
+++ src/lib/services/db/mongodb.ts
@@ -27,5 +27,6 @@ if (process.env['NODE_ENV'] === 'development') {
 }
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
+console.log('Created Client');
 
 export { clientPromise };
