9c8dfb5eed3f043efeb228a637a9cd197d214c3a
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-20 01:27:00 +0000

    Add temp test KGcodes for database seeding

---
+++ src/lib/services/db/seed/data/test-data.js
@@ -0,0 +1,29 @@
+const kgCodes = [
+  {
+    "latitude": -25.25,
+    "longitude": 46.75,
+    "kgcode": "Af"
+  },
+  {
+    "latitude": -24.75,
+    "longitude": 47.25,
+    "kgcode": "Af"
+  },
+  {
+    "latitude": -24.25,
+    "longitude": -47.25,
+    "kgcode": "Af"
+  },
+  {
+    "latitude": -23.25,
+    "longitude": -149.25,
+    "kgcode": "Af"
+  },
+  {
+    "latitude": -23.25,
+    "longitude": -134.75,
+    "kgcode": "Af"
+  },
+];
+
+export { kgCodes };
