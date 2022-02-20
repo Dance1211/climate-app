1e1b17ab3d84a20d81f26d4957dbe8bc6b216c4e
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-20 17:48:13 +0000

    Update seed function to use updated kg-code type

--- src/lib/services/db/seed/kg-code-seed.ts
+++ src/lib/services/db/seed/kg-code-seed.ts
@@ -1,14 +1,14 @@
-import type { KGCode } from '$lib/types/kg-code';
+import type { KGCodeDB } from '$lib/types/kg-code';
 import type { Collection, Db, MongoClient } from 'mongodb';
 
-export const seed = async (clientConnection: MongoClient, data: Array<KGCode>): Promise<void> => {
+export const seed = async (clientConnection: MongoClient, data: Array<KGCodeDB>): Promise<void> => {
 	// Connect to a specific database
 	const db: Db = clientConnection.db(process.env['MONGODB_COLLECTION_LOCATIONS']);
 
 	await db.dropCollection(process.env['MONGODB_KGCODES']);
 	await db.createCollection(process.env['MONGODB_KGCODES']);
 
-	const kgCodeCollection: Collection<KGCode> = db.collection(process.env['MONGODB_KGCODES']);
+	const kgCodeCollection: Collection<KGCodeDB> = db.collection(process.env['MONGODB_KGCODES']);
 
 	try {
 		await kgCodeCollection.bulkWrite(
