41bbc0352b6703229490d6c6d864884562d04b91
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-21 09:16:09 +0000

    Remove unnecessary client connectoin from index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
-import { clientPromise } from '$lib/services/db/mongodb';
 import runSeed from '$lib/services/db/seed/kg-code-runseed';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function post({ request }: RequestEvent) {
 	console.log('Got a post request!');
-  
-	const clientConnection = await clientPromise;
 
   runSeed();
 
