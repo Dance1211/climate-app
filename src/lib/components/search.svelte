6de8c18b0c22e601bd17a675b42a3c9ac6b197c4
Ben <benwestondigital@gmail.com>
2022-02-21 15:58:09 +0000

    add onsubmit handler

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,30 +1,67 @@
-<script>
-	let locationTerm = '';
-    let homeTerm = '';
+<script lang="ts">
+	type Query = { location: string; home: string };
+
+	let searchQuery: Query = {
+		location: '',
+		home: ''
+	};
+
+    $: console.log(searchQuery)
+
+	const onSubmit = (e): void => {
+        console.log(e);
+		const formData: any = new FormData(e.target);
+		const asString = new URLSearchParams(formData).toString();
+		console.log(asString);
+        // axios request
+        // create new component to render data inside
+	};
 </script>
 
 <svelte:head>
 	<title>Climate Travel App</title>
 </svelte:head>
 
-<div class="Search">
+<main class="Search">
 	<h2>Compare travel destinations by climate.</h2>
 
-	<form>
-		<label for="location">I like the weather in</label>
-		<input type="text" id="location" bind:value={locationTerm} placeholder="City, country or town..." />
-		<label for="home">My location is</label>
-		<input type="text" id="home" bind:value={homeTerm} placeholder="City or town..." />
-		<button type="submit">Search</button>
+	<form on:submit|preventDefault={onSubmit}>
+		<div>
+			<label for="location">I like the weather in</label>
+			<input
+				type="text"
+				name="location"
+				id="location"
+				bind:value={searchQuery.location}
+				placeholder="City, country or town..."
+				required
+			/>
+		</div>
+		<div>
+			<label for="home">My location is</label>
+			<input
+				type="text"
+				name="home"
+				id="home"
+				bind:value={searchQuery.home}
+				placeholder="City or town..."
+			/>
+		</div>
+		<button disabled={!searchQuery.location} type="submit">Search</button>
 	</form>
-</div>
+</main>
 
 <style>
 	form {
-		display: grid;
+		display: flex;
+		flex-direction: column;
 		justify-items: center;
 		align-items: center;
 		grid-template-columns: 1fr;
 		grid-template-rows: auto;
+	}
+	form > div {
+		display: flex;
+		flex-direction: column;
 	}
 </style>
