9c061bb0ed44d61888896715a494b6bb9296b31d
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-22 15:42:05 +0000

    Removed console.log

--- src/lib/services/db/mongodb.ts
+++ src/lib/services/db/mongodb.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const uri = process.env['MONGODB_URI'];
-console.log("uri", uri);
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
