acf705bd5f17119df5c349a73816886abdd285a9
Ben <benwestondigital@gmail.com>
2022-02-22 18:17:12 +0000

    remove console log

--- src/routes/api/latlng/[place_id].ts
+++ src/routes/api/latlng/[place_id].ts
@@ -4,7 +4,6 @@ const apiKey = import.meta.env.VITE_API_KEY;
 export async function get({ params }) {
 	try {
 		const place_id = params.place_id;
-		console.log(place_id);
 		const baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
 		const res = await axios.get(baseUrl, {
 			params: {
@@ -12,7 +11,6 @@ export async function get({ params }) {
 				place_id: place_id
 			}
 		});
-		console.log(res, 'results');
 		const lat = +res.data.results[0].geometry.location.lat.toFixed(5);
 		const lng = +res.data.results[0].geometry.location.lng.toFixed(5);
 		return {
