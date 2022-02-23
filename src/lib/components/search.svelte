c01b0a2d3028bd51972238c92675a0b6d35b00a7
Ben <benwestondigital@gmail.com>
2022-02-23 12:54:35 +0000

    improve comments and add tidy file

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import axios from 'axios';
 	import { goto } from '$app/navigation';
-	const apiKey = import.meta.env.VITE_API_KEY;
 	type Coordinates = null | [lat: number, lng: number];
 
 	let predictionsArr;
@@ -10,15 +9,13 @@
 
 	// Input from user
 	let locationInput: string = '';
-
 	// Destination co-ords received from coordinateFetch
 	let destinationLocation: Coordinates = null;
-
 	// User input co-ords from getCurrentPosition - default is set to Manchester
 	let userLocation: Coordinates = [53.48076, -2.24263];
 
 	/* Event handlers */
-	// Gets co-ords when user submits first form
+	// Gets coordinates when user submits first form
 	const onInitialSubmit = async (): Promise<void> => {
 		await placeIdFetch();
 		await coordinateFetch(placeId);
@@ -29,7 +26,7 @@
 		await coordinateFetch(e.target.value);
 	};
 
-	// Go-to next page when user confirms correct location
+	// Go-to next page when user confirms correct location - passes through location, user location and country as URL params
 	const onConfirmSubmit = (): void => {
 		if (destinationLocation) {
 			const searchUrl = `/searchresults&lat=${destinationLocation[0]}&lng=${destinationLocation[1]}&country=${countryCode}&userlat=${userLocation[0]}&userlng=${userLocation[1]}`;
@@ -40,7 +37,7 @@
 		}
 	};
 
-	// Sets user location on button click
+	// Sets user location on locationButton click
 	const onUserLocation = (): void => {
 		let locationBox = document.getElementById('locationBox');
 		if (navigator.geolocation) {
@@ -51,7 +48,7 @@
 			userLocation = [+position.coords.latitude.toFixed(5), +position.coords.longitude.toFixed(5)];
 			locationBox.innerHTML = 'User location set.';
 		}
-
+		// Writes to page based on the error code
 		function showError(error) {
 			switch (error.code) {
 				case error.PERMISSION_DENIED:
@@ -74,7 +71,7 @@
 		}
 	};
 
-	// Function to Google Maps API to autocomplete a full address from the user's input. Gets the place id and then calls geoCodingFetch to get the lat/long co-ords
+	// Call to Google Maps API to autocomplete a full address from the user's input. Gets the place id and array of predictions (used to build drop-down list)
 	const placeIdFetch = async () => {
 		const res = await axios.get(`/api/destination/${locationInput}`);
 		const { place_id, predictions } = res.data;
@@ -82,7 +79,7 @@
 		predictionsArr = predictions;
 	};
 
-	// Function to Google Geocoding API get lat/long co-ords from the place id that we got through placesAutoComplete
+	// Call to Google Geocoding API get lat/long coordinatess from the place id that was received in placesIdFetch
 	const coordinateFetch = async (place: string): Promise<void> => {
 		const res = await axios.get(`/api/latlng/${place}`);
 		const { coordinates, country } = res.data;
@@ -116,7 +113,8 @@
 		{/if}
 		<button on:click|once={onUserLocation} type="button">Get my location</button>
 		<span id="locationBox" />
-		<button type="submit">Submit</button>
+		<p>Press to give your current location</p>
+		<button id="locationButton" type="submit">Submit</button>
 	</form>
 
 	{#if destinationLocation}
