09c45153f73fe1d2cd46bab7c5356925fc4e0058
Ben <benwestondigital@gmail.com>
2022-02-24 15:58:29 +0000

    add placeinfo api route

---
+++ src/routes/api/placeinfo/[place_id].ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export async function get({ params }) {
+	try {
+		const place_id = params.place_id;
+		const baseUrl = `https://maps.googleapis.com/maps/api/place/details/json`;
+		const res = await axios.get(baseUrl, { params: { key: apiKey, place_id } });
+		const { photos, reviews, rating, user_ratings_total } = res.data.result;
+
+		return {
+			status: 200,
+			body: { photos, reviews, rating, user_ratings_total }
+		};
+	} catch (err) {
+		return {
+			status: 500,
+			body: {
+				err
+			}
+		};
+	}
+}
