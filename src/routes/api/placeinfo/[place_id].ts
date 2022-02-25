f773b821b98dec4427975f2ef81263cea40270dd
Ben <benwestondigital@gmail.com>
2022-02-25 11:34:02 +0000

    amend api return to only pass one photo object

--- src/routes/api/placeinfo/[place_id].ts
+++ src/routes/api/placeinfo/[place_id].ts
@@ -6,12 +6,13 @@ export async function get({ params }) {
 		const place_id = params.place_id;
 		const baseUrl = `https://maps.googleapis.com/maps/api/place/details/json`;
 		const res = await axios.get(baseUrl, { params: { key: apiKey, place_id } });
+		console.log(res.data);
 
 		const { photos, rating, user_ratings_total } = res.data.result;
 
 		return {
 			status: 200,
-			body: { photos, rating, user_ratings_total }
+			body: { photos: photos[0], rating, user_ratings_total }
 		};
 	} catch (err) {
 		return {
