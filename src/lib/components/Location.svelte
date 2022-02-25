46ff4483eb2ba134a5b0477149c5409b8dacc54e
Ben <benwestondigital@gmail.com>
2022-02-25 11:31:58 +0000

    get name from url, call api to get placeid, render name on page

--- src/lib/components/Location.svelte
+++ src/lib/components/Location.svelte
@@ -1,26 +1,43 @@
 <script lang="ts">
 	import axios from 'axios';
 	import { onMount } from 'svelte';
+	let place: string = '';
+	let country: string = '';
+	let place_id: string;
+
+	// get place from url params
 
 	let data;
 
-	let place_id = 'ChIJN1t_tDeuEmsRUsoyG83frY4';
-
-	const infoFetch = async () => {
-		const res = await axios.get(`/api/placeinfo/${place_id}`);
-		return res.data;
-	};
-
 	onMount(async () => {
+		let params = new URLSearchParams(document.location.search);
+		place = params.get('place');
+		country = params.get('country');
+
+		const getPlaceId = async (): Promise<string> => {
+			const res = await axios.get(`api/destination/${place}`)
+			return res.data.place_id;
+		}
+		place_id = await getPlaceId();
+
+		const infoFetch = async () => {
+			const res = await axios.get(`/api/placeinfo/${place_id}`);
+			return res.data;
+		};
 		data = await infoFetch();
-		console.log(data);
+		console.log(data,'data');
 	});
 </script>
 
 <div class="LocationData">
-	<h1>Location Name</h1>
+	<h1>
+		{place.slice(0, 1).toUpperCase() + place.slice(1)}, {country.slice(0, 1).toUpperCase() +
+			country.slice(1)}
+	</h1>
 	{#if data}
-		<p>Rating: {data.rating} based on {data.user_ratings_total} reviews.</p>
+		{#if data.rating}
+			<p>Rating: {data.rating} based on {data.user_ratings_total} reviews.</p>
+		{/if}
 	{:else}
 		<p>loading...</p>
 	{/if}
