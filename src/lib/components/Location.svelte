3f505fc9895c532ab6f666c41ca2dbe14e199207
Ben <benwestondigital@gmail.com>
2022-02-25 15:39:30 +0000

    add dummy data to reduce api call and include Chart component

--- src/lib/components/Location.svelte
+++ src/lib/components/Location.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 	import axios from 'axios';
 	import { onMount } from 'svelte';
+	import Chart from '../components/Chart.svelte';
+	const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
 	let place: string = '';
 	let country: string = '';
 	let place_id: string;
 
 	// get place from url params
+	let dummy = {
+		height: 499,
+		html_attributions: [
+			'<a href="https://maps.google.com/maps/contrib/108917525799035589745">Mike Ledwith</a>'
+		],
+		photo_reference:
+			'Aap_uEBmSUTTQOUbkobEOPS4z8AWnypto8aNTqgOpumo9GPMkisuJtiT5KqZO2GgOuZ-7vtO60G8Tm7NkA8UAhB4iQnEJbOirlxQiTp2ZmOwJXzpVX16uDPFlvZ1H7eYCfAunFTIOT-4155e3Zl9jflZSuQrUJ-3-qq8cCkV2C0xwgYSmP4V',
+		width: 887
+	};
+
+	let src = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${dummy.photo_reference}&key=${apiKey}`;
 
 	let data;
 
@@ -15,17 +29,17 @@
 		country = params.get('country');
 
 		const getPlaceId = async (): Promise<string> => {
-			const res = await axios.get(`api/destination/${place}`)
+			const res = await axios.get(`api/destination/${place}`);
 			return res.data.place_id;
-		}
-		place_id = await getPlaceId();
+		};
+		//	place_id = await getPlaceId();
 
 		const infoFetch = async () => {
 			const res = await axios.get(`/api/placeinfo/${place_id}`);
 			return res.data;
 		};
-		data = await infoFetch();
-		console.log(data,'data');
+		//	data = await infoFetch();
+		console.log(data, 'data');
 	});
 </script>
 
@@ -46,6 +60,11 @@
 	<div class="TempGraph">
 		<h3>Average Temperature</h3>
 	</div>
+	<img {src} alt={place} />
+	{@html dummy.html_attributions}
+
+	<Chart />
+
 	<div class="LocationSuggestions">
 		<h3>You may also like...</h3>
 	</div>
