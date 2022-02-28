a2f337fd2f47538b78c74f24199ac8bd701b0a75
Ben <benwestondigital@gmail.com>
2022-02-28 18:06:12 +0000

    remove api key and remove rating rendering

--- src/lib/components/Location.svelte
+++ src/lib/components/Location.svelte
@@ -2,7 +2,6 @@
 	import axios from 'axios';
 	import { onMount } from 'svelte';
 	import Chart from '../components/Chart.svelte';
-	const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 	let place: string = '';
 	let country: string = '';
@@ -12,23 +11,27 @@
 	let photo_reference: string;
 
 	onMount(async () => {
-		let params = new URLSearchParams(document.location.search);
-		place = params.get('place');
-		country = params.get('country');
+		try {
+			let params = new URLSearchParams(document.location.search);
+			place = params.get('place');
+			country = params.get('country');
 
-		const getPlaceId = async (): Promise<string> => {
-			const res = await axios.get(`api/destination/${place}`);
-			return res.data.place_id;
-		};
-		place_id = await getPlaceId();
+			const getPlaceId = async (): Promise<string> => {
+				const res = await axios.get(`api/destination/${place}`);
+				return res.data.place_id;
+			};
+			place_id = await getPlaceId();
 
-		const infoFetch = async () => {
-			const res = await axios.get(`/api/placeinfo/${place_id}`);
-			return res.data;
-		};
-		data = await infoFetch();
-		photo_reference = data.photos.photo_reference;
-		src = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_reference}&key=AIzaSyC9fUjZpK0R-RE_BarfZmkv25fT3YCUirE`;
+			const infoFetch = async () => {
+				const res = await axios.get(`/api/placeinfo/${place_id}`);
+				return res.data;
+			};
+			data = await infoFetch();
+			photo_reference = data.photos.photo_reference;
+			src = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_reference}&key=ENTERKEYHERE`;
+		} catch (err) {
+			console.log(err);
+		}
 
 		/* This function is used to grab the raw image data. It is commented out because I don't know how to render it onto the page. 		
 		const photoFetch = async () => {
@@ -47,17 +50,9 @@
 	<!-- include climate code here -->
 	<img {src} alt={place} />
 
-	{#if data}
-		{#if data.rating}
-			<p>Rating: {data.rating} based on {data.user_ratings_total} reviews.</p>
-		{/if}
-	{:else}
-		<p>loading...</p>
-	{/if}
-
 	<h2>Location Data</h2>
 	<div class="TempGraph">
-		<Chart />
+		<Chart {place} />
 	</div>
 
 	<div class="LocationSuggestions">
