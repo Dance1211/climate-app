498821d74a0a866a0d85d773ff1d93ba36cfd10c
Ben <benwestondigital@gmail.com>
2022-02-28 18:01:59 +0000

    remove dynamic fetch api url as not working

--- src/lib/components/Chart.svelte
+++ src/lib/components/Chart.svelte
@@ -7,53 +7,61 @@
 	let weather;
 
 	onMount(async () => {
-		const fetchWeather = async () => {
-			try {
-				const res = await axios.get(`api/historical/${place}`);
-				return res.data.tempByMonth;
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		weather = await fetchWeather();
-
-		const myChart = new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: weather.map((x) => x[0]),
-				datasets: [
-					{
-						label: 'Avg Temp by Month',
-						data: weather.map((x) => x[1]),
-						backgroundColor: [
-							'rgba(255, 99, 132, 0.2)',
-							'rgba(54, 162, 235, 0.2)',
-							'rgba(255, 206, 86, 0.2)',
-							'rgba(75, 192, 192, 0.2)',
-							'rgba(153, 102, 255, 0.2)',
-							'rgba(255, 159, 64, 0.2)'
-						],
-						borderColor: [
-							'rgba(255, 99, 132, 1)',
-							'rgba(54, 162, 235, 1)',
-							'rgba(255, 206, 86, 1)',
-							'rgba(75, 192, 192, 1)',
-							'rgba(153, 102, 255, 1)',
-							'rgba(255, 159, 64, 1)'
-						],
-						borderWidth: 1
-					}
-				]
-			},
-			options: {
-				scales: {
-					y: {
-						beginAtZero: true
-					}
+		try {
+			const fetchWeather = async () => {
+				try {
+					//this is not working properly. need to fix the string interpolation for place variable
+					const res = await axios.get(`api/historical/dubai`);
+					return res.data.tempByMonth;
+				} catch (err) {
+					console.log(err);
 				}
+			};
+			weather = await fetchWeather();
+
+			if (weather) {
+				const myChart = new Chart(ctx, {
+					type: 'bar',
+					data: {
+						labels: weather.map((x) => x[0]),
+						datasets: [
+							{
+								label: 'Avg Temp by Month',
+								data: weather.map((x) => x[1]),
+								backgroundColor: [
+									'rgba(255, 99, 132, 0.2)',
+									'rgba(54, 162, 235, 0.2)',
+									'rgba(255, 206, 86, 0.2)',
+									'rgba(75, 192, 192, 0.2)',
+									'rgba(153, 102, 255, 0.2)',
+									'rgba(255, 159, 64, 0.2)'
+								],
+								borderColor: [
+									'rgba(255, 99, 132, 1)',
+									'rgba(54, 162, 235, 1)',
+									'rgba(255, 206, 86, 1)',
+									'rgba(75, 192, 192, 1)',
+									'rgba(153, 102, 255, 1)',
+									'rgba(255, 159, 64, 1)'
+								],
+								borderWidth: 1
+							}
+						]
+					},
+					options: {
+						scales: {
+							y: {
+								beginAtZero: true
+							}
+						}
+					}
+				});
 			}
-		});
+		} catch (err) {
+			console.log(err);
+		}
 	});
 </script>
+
 
 <canvas id="myChart" bind:this={ctx} />
