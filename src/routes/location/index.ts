3f7dce9824cd6f5d1fbab9184b2fd82627318945
Ben <benwestondigital@gmail.com>
2022-03-01 15:43:58 +0000

    refactor api calls to use getPhotoRef model

--- src/routes/location/index.ts
+++ src/routes/location/index.ts
@@ -1,20 +1,15 @@
 import type { LoadInput } from '@sveltejs/kit';
-import axios from 'axios';
+import { getPhotoRef } from '$lib/models/photoreference';
 
 export async function get({ url }: LoadInput) {
 	const place = url.searchParams.get('place');
 	const country = url.searchParams.get('country');
+	const photo_reference = await getPhotoRef(place);
 
-	const getPlaceId = await axios.get(`api/destination/${place}`);
-	/* 	const place_id = getPlaceId.data.place_id;
-
-	const getPhotoRef = await axios.get(`/api/placeinfo/${place_id}`);
-	const photo_reference =getPhotoRef.data.photos.photo_reference;
-    console.log(photo_reference);
- */
+    const src = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_reference}&key=AIzaSyC9fUjZpK0R-RE_BarfZmkv25fT3YCUirE`;
 
 	return {
 		status: 200,
-		body: { key: 'nothing' }
+		body: { src, place, country }
 	};
 }
