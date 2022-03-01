7dfb5258fa3ba8e22765211866cd39fe252c5094
Ben <benwestondigital@gmail.com>
2022-03-01 16:35:55 +0000

    render cities to you may also like section

--- src/routes/location/index.svelte
+++ src/routes/location/index.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import Chart from '$components/Chart.svelte';
-    export let src: string;
-    export let place: string;
-    export let country: string;
-    export let weather;
+	export let src: string;
+	export let place: string;
+	export let country: string;
+	export let weather: any;
+	export let cities: any[];
 </script>
 
 <div class="LocationData">
@@ -15,11 +16,14 @@
 
 	<h2>Location Data</h2>
 	<div class="TempGraph">
-		<Chart {weather}/>
+		<Chart {weather} />
 	</div>
 
 	<div class="LocationSuggestions">
 		<h3>You may also like...</h3>
+		{#each cities.slice(0, 5) as city (city._id)}
+			<p>{city.city}</p>
+		{/each}
 	</div>
 </div>
 
