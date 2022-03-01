a5b0e023d4e00288c1a9778751a8661e5c0902c6
Ben <benwestondigital@gmail.com>
2022-03-01 16:14:47 +0000

    add similar cities func and return value

--- src/routes/location/index.ts
+++ src/routes/location/index.ts
@@ -1,18 +1,24 @@
 import type { LoadInput } from '@sveltejs/kit';
 import { getPhotoRef } from '$lib/models/photoreference';
 import { getHistoricWeather } from '$lib/models/historicWeather';
+import { getCitiesSimilarToLocation } from '$lib/models/cities';
+import { getLatLng } from '$lib/models/getlatlng';
 
 export async function get({ url }: LoadInput) {
 	const place = url.searchParams.get('place');
 	const country = url.searchParams.get('country');
-	const photo_reference = await getPhotoRef(place);
+	const { photo_reference, place_id } = await getPhotoRef(place);
 
-    const src = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_reference}&key=AIzaSyC9fUjZpK0R-RE_BarfZmkv25fT3YCUirE`;
+	const src = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_reference}&key=AIzaSyC9fUjZpK0R-RE_BarfZmkv25fT3YCUirE`;
 
 	const weather = await getHistoricWeather(place);
 
+	const { lat, lng } = await getLatLng(place_id);
+
+	const cities = await getCitiesSimilarToLocation([lng, lat], {});
+
 	return {
 		status: 200,
-		body: { src, place, country, weather }
+		body: { src, place, country, weather, cities }
 	};
 }
