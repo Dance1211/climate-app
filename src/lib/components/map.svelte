e76bff0e89f7e1e662b0b56e2b419d1257106c66
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-03-01 14:44:54 +0000

    initial commit!

---
+++ src/lib/components/map.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<section class="map">
+  HELLO
+</section>
+
+<style>
+  .map {
+    width: 100%;
+    max-width: 100vw;
+    max-height: 100vh;
+    background-image: url("/images/mapBackground.png");
+    background-position: center;
+    background-size: cover;
+    aspect-ratio: 2;
+  }
+</style>
