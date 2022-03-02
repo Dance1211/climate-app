f8fb8c52fb8b33bc48e93c0c18bbe04a7d6eec4e
CH-DD <68435229+CH-DD@users.noreply.github.com>
2022-03-02 18:50:49 +0000

    My Places and My Location page updates - icons & css

--- src/routes/my-locations.svelte
+++ src/routes/my-locations.svelte
@@ -9,18 +9,27 @@
 <main class="text-page my-locations">
 	<section class="text-area">
 		<h1>My Places  <span class="material-icons">
-			location_on
+			place
 			</span> </h1>
 			
 		<!-- test these URLS -->
 		<h3 class="search-item">
-			<a href="/location?place=cairo&country=egypt">Cairo <small>- Egypt</small></a>
+			<a href="/location?place=cairo&country=egypt">
+				<span class="material-icons">place</span>
+				Cairo <small>- Egypt</small>
+			</a>
 		</h3>
 		<h3 class="search-item">
-			<a href="/location?place=lapland&country=finland">Lapland <small>- Finland</small></a>
+			<a href="/location?place=lapland&country=finland">
+				<span class="material-icons">place</span>
+				Lapland <small>- Finland</small>
+			</a>
 		</h3>
 		<h3 class="search-item">
-			<a href="/location?place=berlin&country=germany">Berlin <small>- Germany</small></a>
+			<a href="/location?place=berlin&country=germany">
+				<span class="material-icons">place</span>
+				Berlin <small>- Germany</small>
+			</a>
 		</h3>
 
 
@@ -32,24 +41,37 @@
 
 <style>
 	h1 {
-		padding-bottom: 1rem;
 		border-bottom: 1px dashed var(--col-dividers-borders);
+		padding-bottom: 1rem;
 	}
-	h3 a {
+	h1 .material-icons {
+		margin-left: 1rem;
+	}
+	h3.search-item a {
 		color: var(--col-brand-secondary);
-	}
-	h3 a:hover {
-		color: var(--col-brand-tertiary);
-		opacity: 1;
-	}
-	h3.search-item {
-		margin-bottom: 1rem;
-		padding-bottom: 1rem;
-		border-bottom: 1px dashed var(--col-dividers-borders);
 		font-weight: bold;
 		line-height: 1.5;
+		text-indent: 1rem;
+		display: block;
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+		border-bottom: 1px dashed var(--col-dividers-borders);
+		transition: transform .1s; /* animation for :hover zoom */
 	}
-	h3.search-item small {
+	h3.search-item a .material-icons {
+		margin-right: 0.5rem;
+		top: 4px;
+		position: relative;
+		display: inline;
+	}
+	h3.search-item a:hover {
+		color: var(--col-brand-tertiary);
+		opacity: 1;
+		text-decoration: none;
+		border-bottom: 1px dashed #a5a5a5;
+		transform: scale(1.02); /* zoom effect */
+	}
+	h3.search-item a small {
 		font-weight: normal;
 	}
 
--- src/routes/my-searches.svelte
+++ src/routes/my-searches.svelte
@@ -14,23 +14,40 @@
 			
 		<!-- test these URLS -->
 		<h3 class="search-item">
-			<a href="/searchresults?lat=30.03333&lng=31.23333&userlat=-2.24464&userlng=53.4835" title="View this search">
-				I like the weather in: <br><strong>Cairo - Egypt</strong><br><br>
-				My Location: <br><strong>Manchester - UK</strong>
+			<a href="/searchresults?lat=30.03333&lng=31.23333&userlat=-2.24464&userlng=53.4835" title="View Search Results">
+				<div class="like-location">
+					<span class="material-icons">search</span>
+					I like the weather in: <strong>Cairo - Egypt</strong>
+				</div>
+				<div class="my-location">
+					<span class="material-icons">place</span>
+					My Location: <strong>Manchester - UK</strong>
+				</div>		
 			</a>
 		</h3>
 
 		<h3 class="search-item">
-			<a href="/searchresults?lat=67.92223&lng=26.50464&userlat=-2.24464&userlng=53.4835" title="View this search">
-				I like the weather in: <br><strong>Lapland - Finland</strong><br><br>
-				My Location: <br><strong>Manchester - UK</strong>
+			<a href="/searchresults?lat=67.92223&lng=26.50464&userlat=-2.24464&userlng=53.4835" title="View Search Results">
+				<div class="like-location">
+					<span class="material-icons">search</span>
+					I like the weather in: <strong>Lapland - Finland</strong>
+				</div>
+				<div class="my-location">
+					<span class="material-icons">place</span>
+					My Location: <strong>Manchester - UK</strong>
+				</div>		
 			</a>
 		</h3>
 		<h3 class="search-item">
-			<a href="/searchresults?lat=52.52000&lng=13.40495&userlat=-2.24464&userlng=53.4835" title="View this search">
-				I like the weather in: <br>
-				<strong>Berlin - Germany</strong><br><br>
-				My Location: <br><strong>Manchester - UK</strong>
+			<a href="/searchresults?lat=52.52000&lng=13.40495&userlat=-2.24464&userlng=53.4835" title="View Search Results">
+				<div class="like-location">
+					<span class="material-icons">search</span>
+					I like the weather in: <strong>Berlin - Germany</strong>
+				</div>
+				<div class="my-location">
+					<span class="material-icons">place</span>
+					My Location: <strong>Manchester - UK</strong>
+				</div>	
 			</a>
 		</h3>
 
@@ -45,14 +62,14 @@
 	h1 {
 		padding-bottom: 1rem;
 		border-bottom: 1px dashed var(--col-dividers-borders);
-		
+	}
+	h1 .material-icons {
+		margin-left:1rem;
 	}
 	h3 a {
 		color: var(--col-brand-secondary);
 	}
 	h3.search-item {
-		margin-bottom: 1rem;
-		padding-bottom: 1rem;
 		border-bottom: 1px dashed var(--col-dividers-borders);
 		font-weight: normal;
 		line-height: 1.5;
@@ -61,4 +78,35 @@
 		font-weight: bold;
 		/* color: var(--col-brand-primary); */
 	}
+
+	h3.search-item a {
+		color: var(--col-brand-secondary);
+		line-height: 1.5;
+		display: block;
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+		border-bottom: 1px dashed var(--col-dividers-borders);
+		transition: transform .1s; /* animation for :hover zoom */
+	}
+	h3.search-item a .material-icons {
+		margin-right: 0.5rem;
+		top: 4px;
+		position: relative;
+		display: inline;
+	}
+	h3.search-item a div {
+		text-indent: 1rem;
+		display: block;
+	}
+	h3.search-item a div.like-location {
+		margin-bottom: 0.5rem;
+	}
+	h3.search-item a:hover {
+		color: var(--col-brand-tertiary);
+		opacity: 1;
+		text-decoration: none;
+		border-bottom: 1px dashed #a5a5a5;;
+		transform: scale(1.02); /* zoom effect */
+	}
+
 </style>
