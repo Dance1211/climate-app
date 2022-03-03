3d9385d0a59ce36f270b8a5fa2d10ea40d9e90d5
Ben <benwestondigital@gmail.com>
2022-03-03 10:24:38 +0000

    hide api key and remove console logs

--- src/routes/searchresults/index.ts
+++ src/routes/searchresults/index.ts
@@ -3,7 +3,6 @@ import { getCitiesSimilarToLocation } from '$lib/models/cities';
 import { getWeather } from '$lib/models/weather';
 import type { City, CityDB } from '$lib/types/cities';
 import { getPhotoRef } from '$lib/models/photoreference';
-const photoKey = import.meta.env.VITE_API_KEY;
 
 export async function get({ url }: LoadInput) {
 	const lat = +url.searchParams.get('lat') || 53.48;
@@ -16,9 +15,7 @@ export async function get({ url }: LoadInput) {
 	} catch (error) {
 		console.log(error);
 	}
-
-	console.dir(cities, { depth: 3 });
-
+	
 	const combinedData = await Promise.all(
 		cities.map(async (city) => {
 			const cityWeather = await getWeather(city.city_ascii);
